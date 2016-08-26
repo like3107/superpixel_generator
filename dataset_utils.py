du: fixed bug for perfect play (was always False) train scripts: added ahp_rand utils: averaged error plot

dataset_utils.py
@@ -255,6 +255,7 @@ class HoneyBatcherPredict(object):
 
         # private
         self.perfect_play = perfect_play
+
         self.global_batch = None  # includes padding, nn input
         self.global_raw = None
         self.global_claims = None  # includes padding, tri-map, inp
@@ -264,7 +265,6 @@ class HoneyBatcherPredict(object):
         self.priority_queue = None
         self.coordinate_offset = np.array([[-1,0],[0,-1],[1,0],[0,1]],dtype=np.int)
         self.direction_array = np.arange(4)
-        self.perfect_play = False
         self.error_indicator_pass = np.zeros((batch_size))
         # debug
         self.max_batch = 0
@@ -470,6 +470,7 @@ class HoneyBatcherPredict(object):
             # check whether already pulled
             assert (self.global_claims[b, center[0], center[1]] == 0)
             self.global_claims[b, center[0], center[1]] = Id
+
             self.global_heightmap_batch[b,
                                         center[0] - self.pad,
                                         center[1] - self.pad] = height
@@ -538,7 +539,8 @@ class HoneyBatcherPath(HoneyBatcherPredict):
                  global_edge_len=110, patch_len=40, padding_b=False,
                  find_errors_b=True, clip_method='clip',
                  timos_seeds_b=True, slices=None,
-                 scale_height_factor=None, perfect_play=False):
+                 scale_height_factor=None, perfect_play=False,
+                 add_height_b=False):
         super(HoneyBatcherPath, self).__init__(membranes=membranes,
                                                membrane_key=membrane_key,
                                                raw=raw, raw_key=raw_key,
@@ -593,6 +595,7 @@ class HoneyBatcherPath(HoneyBatcherPredict):
                                             self.pad:-self.pad]
 
         # private
+        self.add_height_b = add_height_b
         self.global_directionmap_batch = None  # no padding
         self.global_label_batch = None  # no padding
         self.global_height_gt_batch = None  # no padding
@@ -679,24 +682,36 @@ class HoneyBatcherPath(HoneyBatcherPredict):
         raw_batch, centers, ids = super(HoneyBatcherPath, self).get_batches()
         gts = np.zeros((self.bs, 4, 1, 1), dtype='float32')
         for b in range(self.bs):
-            gts[b, :, 0, 0] = self.get_adjacent_heights(centers[b], b)
+            if self.add_height_b:
+                gts[b, :, 0, 0] = self.get_adjacent_heights(centers[b], b)
+            else:
+                gts[b, :, 0, 0] = self.get_adjacent_heights(centers[b], b,
+                                                            ids[b])
         assert (not np.any(gts < 0))
         assert (np.any(np.isfinite(raw_batch)))
         assert (not np.any(raw_batch < 0))
         assert (np.any(np.isfinite(centers)))
         assert (np.any(np.isfinite(ids)))
         assert (np.any(np.isfinite(gts)))
-
         return raw_batch, gts, centers, ids
 
-    def get_adjacent_heights(self, seed, batch):
+    def get_adjacent_heights(self, seed, batch, Id=None):
         seeds_x, seeds_y, _ = self.get_cross_coords_offset(seed)
+        # boundary conditions
         assert (np.any(seeds_x >= 0) or np.any(seeds_y >= 0))
         assert (np.any(self.rl - self.pl > seeds_x) or
                 np.any(self.rl - self.pl > seeds_y))
-        # boundary conditions
         ground_truth = \
             self.global_height_gt_batch[batch, seeds_x, seeds_y].flatten()
+        if Id is not None:
+            mask = [self.global_label_batch[batch,
+                                           seeds_x,
+                                           seeds_y] != \
+                    self.global_id2gt[batch][Id]]
+            if np.any(mask):
+                ground_truth[mask] = \
+                    (self.pad + 1) * self.scaling + \
+                    np.random.randint(0, self.scaling)
         return ground_truth
 
     def get_centers_from_queue(self):

train_scripts.py
@@ -107,7 +107,8 @@ def train_script_v1(options):
            find_errors_b=options.fine_tune_b,
            clip_method=options.clip_method, timos_seeds_b=options.timos_seeds_b,
            scale_height_factor=options.scale_height_factor,
-           perfect_play=options.perfect_play)
+           perfect_play=options.perfect_play,
+           add_height_b=options.ahp_rand)
     bm.init_batch()
 
     if options.val_b:
@@ -245,11 +246,14 @@ def train_script_v1(options):
                 # fine tuning
                 print 'Finetuning...'
                 ft_iteration += 1
-                batch_ft, dir_ft, mem_choice_ft = Memento_ft.get_batch(options.batch_size_ft+options.exp_ft_bs)
+                batch_ft, dir_ft, mem_choice_ft = \
+                    Memento_ft.get_batch(options.batch_size_ft + options.exp_ft_bs)
 
                 if options.augment_ft:
-                    batch_ft_t1, dir_t1 = du.augment_batch(batch_ft[:,0], direction=dir_ft[:,0])
-                    batch_ft_t2, dir_t2 = du.augment_batch(batch_ft[:,1], direction=dir_ft[:,1])
+                    batch_ft_t1, dir_t1 = du.augment_batch(batch_ft[:,0],
+                                                           direction=dir_ft[:,0])
+                    batch_ft_t2, dir_t2 = du.augment_batch(batch_ft[:,1],
+                                                           direction=dir_ft[:,1])
                     batch_ft = np.concatenate((batch_ft_t1, batch_ft_t2), axis=0)
                     batch_dir_ft = np.concatenate((dir_t1, dir_t2), axis=0)
                 else:
@@ -291,7 +295,7 @@ def train_script_v1(options):
                         bm_val.init_batch()
                     free_voxel = free_voxel_empty
 
-        # pretraining
+        # pre-training
         if iteration % 10 == 0 and iteration < options.pre_train_iter:
 
             if options.add_height_penalty and \
@@ -304,9 +308,11 @@ def train_script_v1(options):
                 # start using exp replay only after #options.exp_warmstart iterations
                 if options.exp_warmstart < iteration:
                     if options.exp_height:
-                        membrane, gt, mem_choice = Memento.get_evenheight_batch(options.batch_size+options.exp_bs)
+                        membrane, gt, mem_choice = \
+                            Memento.get_evenheight_batch(options.batch_size+options.exp_bs)
                     else:
-                        membrane, gt, mem_choice = Memento.get_batch(options.batch_size+options.exp_bs)
+                        membrane, gt, mem_choice = \
+                            Memento.get_batch(options.batch_size+options.exp_bs)
 
 
             if options.augment_pretraining:
@@ -370,7 +376,7 @@ def train_script_v1(options):
 
             if options.save_net_b:
                 iterations.append(iteration)
-                losses[0].append(loss_train)
+                losses[0].append(float(loss_train))
                 losses[1].append(loss_train_no_reg)
                 losses[2].append(loss_valid)
                 u.plot_train_val_errors(
@@ -439,6 +445,7 @@ def get_options():
                                       action='store_false')
     p.add('--scale_height_factor', default=100,type=float)
     p.add('--ahp', dest='add_height_penalty', action='store_true')
+    p.add('--ahp_rand', action='store_true')
 
 
     # fine-tuning