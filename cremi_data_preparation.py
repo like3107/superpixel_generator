5cc2767f1c6016f90013eb13065b8f333de7648f
Steffen Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2016-11-11 15:56:46 +0100

    add: export gt boundary in CREMI preparation

--- cremi_data_preparation.py
+++ cremi_data_preparation.py
@@ -3,6 +3,8 @@ import h5py
 import utils as u
 import data_provider as dp
 import numpy as np
+from scipy.ndimage import convolve
+import progressbar
 
 class indexgetter():
     def __init__(self, z_length):
@@ -34,7 +36,6 @@ if __name__ == '__main__':
         total_z_lenght = 0 
         for ds in datasets:
             with h5py.File(options.label_path%ds,"r") as f:
-                print f.keys()[0]
                 z_lenght = f[f.keys()[0]].shape[0]
                 y_lenght = f[f.keys()[0]].shape[1]
                 x_lenght = f[f.keys()[0]].shape[2]
@@ -49,6 +50,11 @@ if __name__ == '__main__':
                                 dtype=np.uint64)
         height_data  = np.empty((total_z_lenght,y_lenght,x_lenght),
                                 dtype=np.float32)
+        boundary_data  = np.empty((total_z_lenght,y_lenght,x_lenght),
+                                dtype=np.float32)
+
+        print "processing ",name," sets ",sl.keys()
+        bar = progressbar.ProgressBar(max_value=total_z_lenght)
 
         for ds in datasets:
 
@@ -57,8 +63,6 @@ if __name__ == '__main__':
             lp = options.label_path%ds
             hp = options.height_gt_path%ds
 
-
-            print "loading datasets"
             membrane = dp.load_h5(mp)[0]
             raw = dp.load_h5(rp)[0]
             raw /= 256. - 0.5
@@ -66,22 +70,23 @@ if __name__ == '__main__':
             label = dp.load_h5(lp)[0]
             z_lenght_in = raw.shape[0]
      
-            # make a list of padded indices
-            # (which cover the boundary cases for the later slicing)
-
-
             gz = indexgetter(z_lenght)
             for z in range(z_lenght)[sl[ds]]:
-                print "\rz",z,"i",i
                 for j in [-1,0,1]:
-                    print raw.shape,input_data.shape, gz(z,j)
                     input_data[i,1+j] = raw[gz(z,j)]
                     input_data[i,4+j] = membrane[gz(z,j)]
 
                 label_data[i] = label[gz(z,0)]
+
+                gx = convolve(label_data[i] + 1, np.array([-1., 0., 1.]).reshape(1, 3))
+                gy = convolve(label_data[i] + 1, np.array([-1., 0., 1.]).reshape(3, 1))
+                boundary_data[i] = np.float32((gx ** 2 + gy ** 2) > 0)
+
                 height_data[i] = height[gz(z,0)]
                 i += 1
+                bar.update(i)
 
+        print "writing data to files"
 
         dp.save_h5(options.input_data_path+"label_CREMI_%s.h5"%name,'data',
                          data=label_data, overwrite='w')
@@ -91,3 +96,6 @@ if __name__ == '__main__':
 
         dp.save_h5(options.input_data_path+"height_CREMI_%s.h5"%name,'data',
                          data=height_data, overwrite='w')
+
+        dp.save_h5(options.input_data_path+"boundary_CREMI_%s.h5"%name,'data',
+                         data=boundary_data, overwrite='w')
