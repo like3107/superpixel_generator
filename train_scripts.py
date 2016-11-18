f3ee6a6accc44655c743955cc968fb8a2d16ac66
Steffen-Wolf <useroffline@bigheron.hci.uni-heidelberg.de>
2016-11-18 14:53:04 +0100

    add: base for recursive prediciton method in du

--- train_scripts.py
+++ train_scripts.py
@@ -212,7 +212,8 @@ class FusionPokemonTrainer(PokemonTrainer):
 
 class FinePokemonTrainer(PokemonTrainer):
     def init_BM(self):
-        self.BM = du.HoneyBatcherPath
+    	print "using edge based prediction method"
+        self.BM = du.HoneyBatcherE
 
     def define_loss(self):
         self.loss = self.builder.get_loss('updates_hydra_v8')
@@ -341,7 +342,8 @@ class SpeedyPokemonTrainer(FinePokemonTrainer):
 
 class FCFinePokemonTrainer(FinePokemonTrainer):
     def init_BM(self):
-        self.BM = du.HoneyBatcherPath
+    	print "using edge based prediction"
+        self.BM = du.HoneyBatcherE
         self.images_counter = -1
 
     def define_loss(self):
