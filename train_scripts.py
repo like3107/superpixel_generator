17770ff2018f488d829b253def7e9ab36a893292
Steffen-Wolf <steffen.wolf@iwr.uni-heidelberg.de>
2017-01-19 23:26:26 +0100

    fix: slicing of static and dynamic input at index options.claim_channels instead of hardcoded 2

--- train_scripts.py
+++ train_scripts.py
@@ -638,7 +638,8 @@ class FCRecFinePokemonTrainer(FCFinePokemonTrainer):
         batch_inits = exp.flatten_stack(exp.stack_batch(rnn_hiddens_e1, rnn_hiddens_e2)).astype(np.float32)
         batch_ft = exp.flatten_stack(exp.stack_batch(error_b_type1, error_b_type2)).astype(np.float32)
 
-        outs = self.builder.loss_train_fine_f(batch_ft[:, :2, :, :], batch_ft[:, 2:, :, :], batch_inits,
+        outs = self.builder.loss_train_fine_f(batch_ft[:, :self.options.claim_channels, :, :],
+                                              batch_ft[:, self.options.claim_channels:, :, :], batch_inits,
                                               batch_mask_ft, options.backtrace_length, grad_weights)
         ft_loss_train, individual_loss_fine, heights, grad_mean, grad_std = outs[:5]
         grads_new = outs[5:]
@@ -1044,7 +1045,7 @@ if __name__ == '__main__':
             trainer.val_bm.set_preselect_batches([12, 101, 53, 98, 138, 60, 20, 131, 35, 119][:trainer.val_bm.bs])
 
 
-        last_val_epoch = -1
+        last_val_epoch = 0
         while not trainer.converged():
             if trainer.val_bm is not None \
                                 and trainer.epoch % options.save_counter == 0 \
