evaluate_net:     fixed loading of options train_scripts:    added weighting gradietns by importance trainer_config parser:     flag for weighting by importance

evaluate_cremi.py
@@ -3,6 +3,7 @@ from trainer_config_parser import get_options
 import time
 import os
 import numpy as np
+import re
 
 def pred_wrapper(options, slices, gpu):
     import evaluate_net
@@ -11,34 +12,20 @@ def pred_wrapper(options, slices, gpu):
     options.slices = slices
     pred = evaluate_net.Predictor(options)
     pred.predict()
-    save_slice_path = options.save_net_path \
-                    + "/slice_%04i_%04i.h5"%(slices[0],slices[-1])
-    save_h5(save_slice_path, 'data',
-        data=pred.bm.global_claims[:, pred.bm.pad:-pred.bm.pad,
-                                      pred.bm.pad:-pred.bm.pad],
-        overwrite='w')
-
-    save_height_path = options.save_net_path \
-                    + "/height_%04i_%04i.h5"%(slices[0],slices[-1])
-    save_h5(save_height_path, 'data',
-        data=pred.bm.global_heightmap_batch,
-        overwrite='w')
-
-
-    save_pred_nq_path = options.save_net_path \
-                    + "/pred_nq_path_%04i_%04i.h5"%(slices[0],slices[-1])
-    save_h5(save_pred_nq_path, 'data',
-            data=pred.bm.global_prediction_map_nq,
+    save_slice_path = options.validation_save_path + "/slice_%04i_%04i.h5"%(slices[0],slices[-1])
+    save_h5(save_slice_path, 'data', data=pred.bm.global_claims[:, pred.bm.pad:-pred.bm.pad, pred.bm.pad:-pred.bm.pad],
             overwrite='w')
 
-    save_slice_path = options.save_net_path \
-                    + "/baseline_%04i_%04i.h5"%(slices[0],slices[-1])
-    save_h5(save_slice_path, 'data',
-            data=pred.bm.get_ws_segmentation(),
-            overwrite='w')
+    save_height_path = options.validation_save_path + "/height_%04i_%04i.h5"%(slices[0],slices[-1])
+    save_h5(save_height_path, 'data', data=pred.bm.global_heightmap_batch, overwrite='w')
 
-def concat_h5_in_folder(path_to_folder, slice_size, n_slices, edge_len,
-                        base_file_name='slice'):
+    save_pred_nq_path = options.validation_save_path  + "/pred_nq_path_%04i_%04i.h5"%(slices[0],slices[-1])
+    save_h5(save_pred_nq_path, 'data', data=pred.bm.global_prediction_map_nq, overwrite='w')
+
+    save_slice_path = options.validation_save_path + "/baseline_%04i_%04i.h5"%(slices[0],slices[-1])
+    save_h5(save_slice_path, 'data', data=pred.bm.get_ws_segmentation(), overwrite='w')
+
+def concat_h5_in_folder(path_to_folder, slice_size, n_slices, base_file_name='slice'):
     import glob
     from data_provider import load_h5, save_h5
     files = sorted(glob.glob(path_to_folder + '/' + base_file_name + '*.h5'))
@@ -48,8 +35,7 @@ def concat_h5_in_folder(path_to_folder, slice_size, n_slices, edge_len,
     le_final = np.zeros(fin_shape, dtype=initial.dtype)
     for start, file in zip(range(0, n_slices, slice_size), files):
         le_final[start:start + slice_size, :, :] = load_h5(file)[0]
-    save_h5(path_to_folder + '/'+base_file_name+'_concat.h5', 'data', data=le_final,
-               overwrite='w')
+    save_h5(path_to_folder + '/'+base_file_name+'_concat.h5', 'data', data=le_final, overwrite='w')
 
 
 def evaluate_h5_files(prediction_path, gt_path, name, options):
@@ -60,7 +46,7 @@ def evaluate_h5_files(prediction_path, gt_path, name, options):
                              offset_xy=int(fov)/2, start_z=options.start_slice_z,
                              n_z=options.slices_total,
                              gel=options.global_edge_len)
-    f = open(options.save_net_path+'/'+name+'results.txt', 'w')
+    f = open(options.validation_save_path+'/'+name+'results.txt', 'w')
     f.write(results)
     f.close()
     print "####################################################"
@@ -87,17 +73,22 @@ if __name__ == '__main__':
         gpus = ["gpu%i"%i for i in range(4)]
     if not os.path.exists(options.save_net_path):
         os.makedirs(options.save_net_path)
-    
-    for i, start in enumerate(range(start_z,start_z+total_z_lenght, options.batch_size)):
-        g = gpus[i%4]
+    net_number = re.search(r'net_\d*', options.load_net_path).group()
+    options.validation_save_path = options.save_net_path + '/validation_%s/' % net_number
+
+    if not os.path.exists(options.validation_save_path):
+        os.mkdir(options.validation_save_path)
+
+    for i, start in enumerate(range(start_z, start_z+total_z_lenght, options.batch_size)):
+        g = gpus[i % 4]
         # processes.append(Process(
         #     target=pred_wrapper,
         #     args=(q, options, range(start,start+options.batch_size), g)))
         pool.apply_async(pred_wrapper,
-            args=(options, range(start,start+options.batch_size), g))
-        time.sleep(8)           # for GPU claim
+            args=(options, range(start, start+options.batch_size), g))
         # debug
-        # pred_wrapper(q, options, range(start, start + options.batch_size), g)
+        # pred_wrapper(options, range(start, start + options.batch_size), g)
+        time.sleep(8)  # for GPU claim
 
     # for p in processes:
     #     p.start()
@@ -116,13 +107,9 @@ if __name__ == '__main__':
         options.global_edge_len -= 70
 
     for bn in ["slice", "height", "pred_nq_path", "baseline"]:
-        concat_h5_in_folder(options.save_net_path,
-                        options.batch_size,
-                        total_z_lenght,
-                        options.global_edge_len,
-                        base_file_name=bn)
+        concat_h5_in_folder(options.validation_save_path, options.batch_size, total_z_lenght, base_file_name=bn)
 
     
     for name in ["slice", "baseline"]:
-        prediction_path = options.save_net_path + '/'+name+'_concat.h5'
+        prediction_path = options.validation_save_path + '/'+name+'_concat.h5'
         evaluate_h5_files(prediction_path, options.label_path, name, options)

train_scripts.py
@@ -494,7 +494,7 @@ class FCRecFinePokemonTrainer(FCFinePokemonTrainer):
         rnn_mask = np.ones((bm.bs*4, sequ_len), dtype=np.float32)
         hiddens = np.repeat(hiddens, 4, axis=0).astype(np.float32)
         height_probs, hidden_out = self.builder.probs_f_fc(inputs[:, :self.options.claim_channels],
-                                                                  precomp_input_sliced,  hiddens, rnn_mask, 1)
+                                                           precomp_input_sliced,  hiddens, rnn_mask, 1)
 
         # if np.all(centers == self.debug_pos):
         #     verbose = True
@@ -633,7 +633,7 @@ class FCRecFinePokemonTrainer(FCFinePokemonTrainer):
         #                                       (self.iterations))
         error_b_type1, error_b_type2, rnn_mask_e1, rnn_mask_e2, rnn_hiddens_e1, rnn_hiddens_e2 = \
             self.bm.reconstruct_path_error_inputs(backtrace_length=options.backtrace_length)
-        grad_weights = self.weight_by_dist(RI_error=self.train_eval)
+        grad_weights = self.weight_gradients(RI_error=self.train_eval)
         batch_mask_ft = exp.flatten_stack(exp.stack_batch(rnn_mask_e1, rnn_mask_e2)).astype(np.float32)
         batch_inits = exp.flatten_stack(exp.stack_batch(rnn_hiddens_e1, rnn_hiddens_e2)).astype(np.float32)
         batch_ft = exp.flatten_stack(exp.stack_batch(error_b_type1, error_b_type2)).astype(np.float32)
@@ -650,7 +650,7 @@ class FCRecFinePokemonTrainer(FCFinePokemonTrainer):
                 g += gn
         return grad_mean, grad_std
 
-    def weight_by_dist(self, RI_error=None):
+    def weight_gradients(self, RI_error=None):
         n_err = len(self.bm.error_selections[0]) * 2 / self.options.backtrace_length
         weights = np.ones(n_err, dtype=np.float32)
         if self.options.weight_by_distance_b:
@@ -663,6 +663,13 @@ class FCRecFinePokemonTrainer(FCFinePokemonTrainer):
                     weights[i] = np.exp(dist*0.05)
         if self.options.weight_by_RI_b:
             weights *= RI_error
+        if self.options.weight_by_importance:
+            i = -1
+            for es, e_type in zip(self.bm.error_selections, ['e1', 'e2']):
+                for e in es[::self.options.backtrace_length]:
+                    i += 1
+                    weights[i] *= e['importance']
+            weights /= np.max(weights)
         return weights
 
     def draw_loss(self, ft_loss_train, ft_loss_noreg, counter=None, name='ft_training'):
@@ -866,7 +873,7 @@ class FCRecMasterFinePokemonTrainer(FCRecFinePokemonTrainer):
                             batch_inits = np.array(h5f['batch_inits'])
                             batch_mask_ft = np.array(h5f['batch_mask_ft'])
                             length = h5f['options.backtrace_length'].value
-                            weights = self.weight_by_dist()
+                            weights = self.weight_gradients()
 
                             ft_loss_train, individual_loss_fine, heights, grad_mean, grad_std = \
                                     self.builder.loss_train_fine_f(batch_ft1, batch_ft2, batch_inits, batch_mask_ft,