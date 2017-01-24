b30de78778183a6c177406baf091b0f4aaf0817d
liory <lukas.schott@gmail.com>
2017-01-24 14:22:23 +0100

    commented old versioned files

--- trainer_config_parser.py
+++ trainer_config_parser.py
@@ -39,8 +39,8 @@ def get_options(script='training', ignore_config=False):
     p.add('--seed_method', type=str, default="timo",
           help='available metods: gt, timo, grid',
           dest='seed_method')
-    p.add('--s_minsize',type=int, default=0)    
-    p.add('--input_data_path',type=str, default="None")
+    p.add('--s_minsize', type=int, default=0)
+    p.add('--input_data_path', type=str, default="None")
 
     # valid data paths
     def_valid_version = 'first_repr'
@@ -92,11 +92,11 @@ def get_options(script='training', ignore_config=False):
 
     # fine-tuning
     p.add('--batch_size_ft', default=4, type=int)
-    p.add('--reset_after_fine_tune', action='store_true')
-    p.add('--no-ft', dest='fine_tune_b', action='store_false')
-    p.add('--reset-ft', dest='rs_ft', action='store_true')
-    p.add('--reset_pretraining', dest='reset_pretraining', action='store_true')
-    p.add('--margin', default=0.5, type=float)
+    # p.add('--reset_after_fine_tune', action='store_true')
+    # p.add('--no-ft', dest='fine_tune_b', action='store_false')
+    # p.add('--reset-ft', dest='rs_ft', action='store_true')
+    # p.add('--reset_pretraining', dest='reset_pretraining', action='store_true')
+    # p.add('--margin', default=0.5, type=float)
     p.add('--no-aug-ft', dest='augment_ft', action='store_false')
     p.add('--optimizer', default="nesterov", type=str)
     p.add('--stochastic_update_b', default=False, action='store_true')
@@ -105,18 +105,18 @@ def get_options(script='training', ignore_config=False):
 
     # experience replay
     # clip_method="exp20"
-    p.add('--exp_bs', default=16, type=int)
-    p.add('--exp_ft_bs', default=8, type=int)
-    p.add('--exp_warmstart', default=1000, type=int)
-    p.add('--exp_acceptance_rate', default=0.1, type=float)
-    p.add('--no-exp_height', dest='exp_height', action='store_false')
-    p.add('--no-exp_save', dest='exp_save', action='store_false')
-    p.add('--exp_mem_size', default=20000, type=int)
-    p.add('--exp_load', default="None", type=str)
-    p.add('--no-exp_loss', dest='exp_loss', action='store_false')
-    p.add('--exp_wlast', default=1., type=float)
-
-    p.add('--max_iter', default=10000000000000, type=int)
+    # p.add('--exp_bs', default=16, type=int)
+    # p.add('--exp_ft_bs', default=8, type=int)
+    # p.add('--exp_warmstart', default=1000, type=int)
+    # p.add('--exp_acceptance_rate', default=0.1, type=float)
+    # p.add('--no-exp_height', dest='exp_height', action='store_false')
+    # p.add('--no-exp_save', dest='exp_save', action='store_false')
+    # p.add('--exp_mem_size', default=20000, type=int)
+    # p.add('--exp_load', default="None", type=str)
+    # p.add('--no-exp_loss', dest='exp_loss', action='store_false')
+    # p.add('--exp_wlast', default=1., type=float)
+    #
+    # p.add('--max_iter', default=10000000000000, type=int)
     p.add('--no_bash_backup', action='store_true')
     p.add('--lowercomplete_e', default=0., type=float)
 
