34b296c2f979ded5f28d955234f1ed8be4aa6c81
liory <lukas.schott@gmail.com>
2017-02-06 18:34:23 +0100

    toy data edge detection

--- test2.py
+++ test2.py
@@ -1,14 +1,14 @@
 import copy
 from os import system
-
+from os import sys
 
 a = open('./../data/config/validation_validtmp.conf', 'r').read()
 
-sigmas = [3, 6, 9]
-holes = [0, 10]
+sigmas = [6]
+holes = [10]
 versions = ['train', 'test', 'valid']
 start_slice = [0, 0, 0]
-slices_total = [10, 10, 100]
+slices_total = [29000, 1000, 100]
 c = copy.copy(a.splitlines())
 
 
@@ -19,23 +19,23 @@ for sigma in sigmas:
             b = open(config, 'w')
             for i, line in enumerate(c):
                 if 'net_name' in line:
-                    c[i] = 'train_version = toy_nh%i_sig%i_%s' %(n_holes, sigma, version)
+                    c[i] = 'net_name = toy_nh%i_sig%i_%s' %(n_holes, sigma, version)
                 if 'slices_total' in line:
                     c[i] = 'start_slice_z         =            %i' % start
                     c[i] = 'slices_total          =            %i' % total
                 if 'gpu' in line:
-                    c[i] = 'gpu = gpu0'
+                    c[i] = 'gpu = gpu4'
                 if 'load_net_path' in line:
-                    c[i] = 'load_net_path =./../data/nets/toy_nh%i_sig%i/nets/net_2000' % (n_holes, sigma)
+                    c[i] = 'load_net_path =./../data/nets/toy_nh%i_sig%i/nets/net_4000' % (n_holes, sigma)
                 if 'train_version' in line:
                     c[i] = 'train_version = toy_nh%i_sig%i_%s'  % (n_holes, sigma, version)
 
             b.write("\n".join(c))
             b.close()
 
-            mypython = '/home/lschott_local/anaconda2/envs/ws_timo/bin/python '
+            mypython = sys.executable
 
-            cmd = mypython + 'evaluate_cremi.py -c ' + config
+            cmd = mypython + ' evaluate_cremi.py -c ' + config
             print cmd
             system(cmd)
 
