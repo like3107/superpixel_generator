68d1a4f0fcd721230ddc89b42edf8d0a646b4a6c
liory <lukas.schott@gmail.com>
2017-03-29 16:10:43 +0200

    before destroying

--- generate_video.py
+++ generate_video.py
@@ -24,13 +24,18 @@ import moviepy.editor as editor
 coordinate_offset = np.array([[-1, 0], [0, -1], [1, 0], [0, 1]], dtype=np.int)
 fov = 35
 sl = 1
+global D_raw
 
 
 
 def msf_plot_i(args):
+
     mlab.clf()
-    print 'args', args
-    i, zoom, time, n_times = args
+    print 'args', args[:-1]
+    i, zoom, time, n_times, D_raw = args
+
+    print 'current step i %i out of %i in percent %f' % (i, len(times), float(i) / n_times * 100)
+
     BM_FILE = '../data/nets/full_validation_path_01/validation_0/bm_116_116.h5'
     # BM_FILE = '../data/nets/fig1_draw_29_without_min/pretrain/serial_00447999'
 
@@ -54,136 +59,45 @@ def msf_plot_i(args):
     MSF_PLOT = False
     DEBUG = True
 
-    with h.File(BM_FILE,'r') as h5f:
-        times = sorted(h5f['global_timemap'][b, fov+ROI[0][0]+1:ROI[0][1]+fov-1, ROI[1][0]+fov+1:ROI[1][1]+fov-1].flatten())
-        print 'current step i %i out of %i in percent %f' % (i, len(times), float(i) / n_times * 100)
+    xlim, ylim = Z.shape
 
-        # debug
-        MAX_TIME = np.max(h5f['global_timemap'][b])
-
-        # START_ZOOM_TIME /= 5
-
-        if DEBUG:
-            times = [100, 1000, 10000, 1000000]
-
-        claims = np.array(h5f['global_claims'])
-        D_raw = np.array(h5f['global_input'][b, 0, fov+ROI_DDD[0][0]:ROI_DDD[0][1]+fov, ROI_DDD[1][0]+fov:ROI_DDD[1][1]+fov])
-        Z = np.min(h5f['global_prediction_map_nq'][b, ROI_DDD[0][0]:ROI_DDD[0][1], ROI_DDD[1][0]:ROI_DDD[1][1]], axis=2)*10
-        Z_total = np.min(h5f['global_prediction_map_nq'][b], axis=2)
+    X, Y = np.mgrid[0:xlim+2, 0:ylim+2]
 
 
-        D_timemap = h5f['global_timemap'][b, fov+ROI_DDD[0][0]:ROI_DDD[0][1]+fov, ROI_DDD[1][0]+fov:ROI_DDD[1][1]+fov]
-        D_claims = claims[b, fov+ROI_DDD[0][0]:ROI_DDD[0][1]+fov, ROI_DDD[1][0]+fov:ROI_DDD[1][1]+fov].astype(int)
-        xlim, ylim = Z.shape
-        X, Y = np.mgrid[0:xlim+2, 0:ylim+2]
+    font = {'family': 'serif',
+            'color':  'darkred',
+            'weight': 'normal',
+            'size': 16,
+                    }
+
+    max_label = np.max(claims[b])
 
 
-        font = {'family': 'serif',
-                'color':  'darkred',
-                'weight': 'normal',
-                'size': 16,
-                        }
+    # bar.update(i)
 
-        max_label = np.max(claims[b])
-        if MSF_PLOT:
-            if i > START_ZOOM_TIME and zoom < FINAL_ZOOM:
-                # zoom = 7 * float(i-START_ZOOM_TIME) / len(times) + 1
-                zoom += ZOOM_STEPS
-                if zoom > FINAL_ZOOM:
-                    zoom = FINAL_ZOOM
-                ROI = get_new_roi(zoom)
-
-            fig, ax = plt.subplots()
-            fig.tight_layout()
-            ax.autoscale()
-            ax.margins(0.1)
-            # print fov+ROI[0][0], ROI[0][1]+fov, ROI[1][0]+fov, ROI[1][1]+fov
-            raw = np.array(h5f['global_input'][b, 0, fov + ROI[0][0]:ROI[0][1] + fov, ROI[1][0] + fov:ROI[1][1] + fov])
-            mimshow = ax.imshow(raw, interpolation=None, cmap='gray', extent=[0, raw.shape[0], 0, raw.shape[1]])
-            # glob_time_map = h5f['global_timemap'][b, fov+ROI[0][0]:ROI[0][1]+fov, ROI[1][0]+fov:ROI[1][1]+fov]
-            # print np.sum(glob_time_map < time)
-            # ax.imshow(glob_time_map, interpolation=None, cmap='gray')
-
-            if zoom < 2:
-                lc = get_masked_MSF(h5f, ROI, time, CM, max_label)
-                ax.add_collection(lc)
-            elif zoom >= 2 and zoom < 3:
-                lc = get_masked_MSF(h5f, ROI, time, CM, max_label)
-                ax.add_collection(lc)
-                masked_claims = get_masked_claims(h5f, ROI, time, claims)
-                print "alpha", 0.8 * ((zoom - 2.))
-                ax.imshow(masked_claims, interpolation='none', cmap=CM, alpha=0.8 * ((zoom - 2.) / 2.5),
-                          clim=(0, max_label))
-            else:
-                masked_claims = get_masked_claims(h5f, ROI, time, claims)
-                ax.imshow(masked_claims, interpolation='none', cmap=CM, alpha=0.8, clim=(0, max_label))
-
-            # ax.text(0.1, 0.9,'learned watershed on CREMI dataset C', ha='left', va='center', transform=ax.transAxes, fontdict=font)
-
-            ax.axis('off')
-            fig.savefig(OUTPUT_PNG % i, bbox_inches='tight', dpi=200)
-            # call(["mogrify", "-crop", "1308x760+0x0", OUTPUT_PNG%i])
-            call(["mogrify", OUTPUT_PNG % i, "-trim", OUTPUT_PNG % i])
-
-            # make height image
-            fig, ax = plt.subplots(facecolor='red')
-            fig.tight_layout()
-
-            ax.text(0.004, 0.95, 'dynamic altitude prediction', ha='left', va='center', transform=ax.transAxes,
-                    fontdict=font)
-
-            # if zoom == 1.:
-            #     ax.imshow(raw, interpolation=None, cmap='gray')
-            ax.axes.get_xaxis().set_ticks([])
-            ax.axes.get_yaxis().set_ticks([])
-            ax.axis('off')
-
-            z_zoom = Z_total[ROI[0][0]:ROI[0][1], ROI[1][0]:ROI[1][1]]
-            pred = get_masked_height(h5f, ROI, time, z_zoom, b=0)
-            ax.imshow(pred, interpolation=None, cmap='gray', clim=(0, max_height))
-            ax.axis('off')
-
-            # ax.axis('off')
-
-            fig.savefig(HEIGHT_PNG % i, bbox_inches='tight', dpi=200, facecolor=fig.get_facecolor())
-            call(["mogrify", HEIGHT_PNG % i, "-trim", HEIGHT_PNG % i])
-
-            # fuse images together
-            call(["convert", OUTPUT_PNG % i, HEIGHT_PNG % i, "-append", COMBINED_PNG % i])
-
-        # bar.update(i)
-
-        if DDD_PLOT:
-            D_raw = np.pad(D_raw, 1, 'constant')
-            fig = plt.figure()
-            ax = fig.gca(projection='3d')
-            Z_masked = np.array(Z)
-            mask = ~ndimage.binary_dilation(D_timemap <= time, iterations=1)
-            Z_masked[mask] = -0.501
+    if DDD_PLOT:
+        D_raw = np.pad(D_raw, 1, 'constant')
+        Z_masked = np.array(Z)
+        mask = ~ndimage.binary_dilation(D_timemap <= time, iterations=1)
+        Z_masked[mask] = -0.501
 
 
-            mimshow = mlab.imshow(D_raw, colormap='gray', extent=[0, D_raw.shape[0], 0, D_raw.shape[1], -0.5, -0.1],
-                                  figure=FIGURE)
+        # mimshow = mlab.imshow(D_raw, colormap='gray', extent=[0, D_raw.shape[0], 0, D_raw.shape[1], -0.5, -0.1],
+        #                       figure=FIGURE)
 
 
-            Z_masked = np.log(Z_masked + 1.00001) * 10
-            Z_masked = np.pad(Z_masked, 1, 'constant', )
-            Z_masked[Z_masked <= 0] = - 0.5001
-            surf = mlab.surf(X, Y, Z_masked, colormap='gray', color=(0.8,0.8,0.8), opacity=0.7, figure=FIGURE)
+        Z_masked = np.log(Z_masked + 1.00001) * 10
+        Z_masked = np.pad(Z_masked, 1, 'constant', )
+        Z_masked[Z_masked <= 0] = - 0.5001
+        surf = mlab.surf(X, Y, Z_masked, colormap='gray', color=(0.8,0.8,0.8), opacity=0.7, figure=FIGURE)
+        #
+        # print "claims...",  D_claims[10,30]
 
+        surf.actor.enable_texture = True
+        surf.actor.tcoord_generator_mode = 'plane'
+        surf.actor.texture = texture_img
 
-            array_2d = CM(D_claims.astype(np.int))         # uint 8 255, 3
-            img = image_from_array(array_2d)
-
-            texture_img = tvtk.Texture(interpolate=1)
-            configure_input_data(texture_img, img)
-            texture_img.input = img
-
-            surf.actor.enable_texture = True
-            surf.actor.tcoord_generator_mode = 'plane'
-            surf.actor.texture = texture_img
-
-            return mlab.screenshot(antialiased=True)
+        return mlab.screenshot(antialiased=True)
 
 def image_from_array(ary):
     """ Create a VTK image object that references the data in ary.
@@ -209,6 +123,7 @@ def image_from_array(ary):
 
     elif dims == 3:
         # 2D array of pixels.
+        print 'is old pip', is_old_pipeline(), (0, sz[0]-1, 0, sz[1]-1, 0, 0)
         if is_old_pipeline():
             img.whole_extent = (0, sz[0]-1, 0, sz[1]-1, 0, 0)
         else:
@@ -302,18 +217,46 @@ if __name__ == '__main__':
 
     ROI_DDD = get_new_roi(6)
 
-    with h.File(BM_FILE,'r') as h5f:
+    with h.File(BM_FILE, 'r') as h5f:
+        times = sorted(h5f['global_timemap'][b, fov + ROI[0][0] + 1:ROI[0][1] + fov - 1,
+                       ROI[1][0] + fov + 1:ROI[1][1] + fov - 1].flatten())
+
+        claims = np.array(h5f['global_claims'])
+        D_raw = np.array(
+            h5f['global_input'][b, 0, fov + ROI_DDD[0][0]:ROI_DDD[0][1] + fov, ROI_DDD[1][0] + fov:ROI_DDD[1][1] + fov]).copy()
+
+        # print 'drqw', D_raw
+        # exit()
+        Z = np.min(h5f['global_prediction_map_nq'][b, ROI_DDD[0][0]:ROI_DDD[0][1], ROI_DDD[1][0]:ROI_DDD[1][1]],
+                   axis=2) * 10
+
+        D_timemap = h5f['global_timemap'][b, fov + ROI_DDD[0][0]:ROI_DDD[0][1] + fov,
+                    ROI_DDD[1][0] + fov:ROI_DDD[1][1] + fov]
+
         times = sorted(h5f['global_timemap'][b, fov+ROI_DDD[0][0]+1:ROI_DDD[0][1]+fov-1, ROI_DDD[1][0]+fov+1:ROI_DDD[1][1]+fov-1].flatten())
         # Z_total = np.log(np.min(h5f['global_prediction_map_nq'][b], axis=2))
         print 'times total', len(times)
         # exit()
-        times = times[::30000] + 20 * [np.max(h5f['global_timemap'][b])]
+        times = times[::6000] + 20 * [np.max(h5f['global_timemap'][b])]
         # times = times[-30:]
+
+
+    D_claims = claims[b, fov+ROI_DDD[0][0]:ROI_DDD[0][1]+fov, ROI_DDD[1][0]+fov:ROI_DDD[1][1]+fov].astype(int)
+    array_2d = CM(D_claims.astype(np.int))         # uint 8 255, 3
+    img = image_from_array(array_2d)
+    print 'array', array_2d[:, :, :3].shape
+    mlab.imshow(array_2d[:, :, 2])
+
+
+    #
+    texture_img = tvtk.Texture(interpolate=1)
+    configure_input_data(texture_img, img)
+    texture_img.input = img
+
 
     # max_height = np.max(Z_total)
     # times = times[0:START_ZOOM_TIME:1] + times[START_ZOOM_TIME:1000:10] + times[1000::30]
     # times = times[100:1000:10]
-
 
     pool = Pool(5)
 
@@ -321,22 +264,30 @@ if __name__ == '__main__':
         # for i, time in enumerate(times):
 
         n_times = len(times)
-        args = [(i, zoom, time, n_t) for zoom, i, time, n_t in zip(n_times * [zoom],
+        args = [(i, zoom, time, n_t, draw) for zoom, i, time, n_t, draw in zip(n_times * [zoom],
                                                          range(n_times),
                                                          times,
-                                                         n_times * [n_times])]
-        print 'list', args
+                                                         n_times * [n_times],
+                                                                   D_raw)]
+        # print 'list', args[]
         print 'len times', len(times), np.max(times)
         #debug
 
+        FIGURE = mlab.figure(1, fgcolor=(0, 0, 0), bgcolor=(1, 1, 1), size=(1200, 1050))
+
+        print args[26][:-1]
+        # msf_plot_i(args[26])
+        msf_plot_i(args[0])
+        mlab.show()
+        exit()
 
         # msf_plot_i(args[0])
-        FIGURE = mlab.figure(1, fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-        animation = editor.VideoClip(lambda  t: msf_plot_i(args[int(t*len(times))]), duration=len(times)/FPS)
+        FIGURE = mlab.figure(1, fgcolor=(0, 0, 0), bgcolor=(1, 1, 1), size=(1200, 1050))
+        animation = editor.VideoClip(lambda t: msf_plot_i(args[int(t*FPS)]), duration=len(times)/FPS)
         animation.write_videofile('crayc.mp4', fps=FPS)
         # animation.write_gif('crayc.gif', fps=24, opt='nq')
         print 'before pool'
-        mlab.show()
+        # mlab.show()
 
         # pool.map(msf_plot_i, args)
 
